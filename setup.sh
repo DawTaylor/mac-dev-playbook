44a3eaca17b0b607b16ff80e5ec283d4c34d366a
Adalberto Taylor <adalbertotaylor84@gmail.com>
2025-11-19 16:23:06 -0300

    Update packages and switch from n to nvm

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 export PATH="$HOME/Library/Python/3.9/bin:/opt/homebrew/bin:$PATH:"
-export N_PREFIX=$HOME/Development/tools/n
 sudo pip3 install --upgrade pip
 pip3 install ansible
 ansible-galaxy install -r requirements.yml
